c704a6044af871e234f2753fa362213173e73516
kal <93663038+kalkidan-hub@users.noreply.github.com>
2023-12-29 17:17:26 +0300

    Added solution for Largest Number

---
+++ Datastructure/sorting/largest-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        if set(nums) == {0}:
+            return str(0)
+        nums = [str(num) for num in nums]
+        # bubble it is
+        for i in range(len(nums)):
+            for j in range(1,len(nums)):
+                if nums[j] + nums[j-1] > nums[j-1] + nums[j]:
+                    nums[j], nums[j-1] = nums[j-1],nums[j]
+        # print(nums)
+
+
+        return "".join(nums)
