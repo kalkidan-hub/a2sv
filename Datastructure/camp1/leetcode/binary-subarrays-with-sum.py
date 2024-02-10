4afe4fb17b28a4b41a41a92bfe2460714c1fbd4d
kal <93663038+kalkidan-hub@users.noreply.github.com>
2024-02-10 14:58:57 +0300

    Add solution for Binary Subarrays With Sum

--- Datastructure/camp1/leetcode/binary-subarrays-with-sum.py
+++ Datastructure/camp1/leetcode/binary-subarrays-with-sum.py
@@ -1,16 +1,27 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        ct = 0
-        prf_sum = [0]
-        ct_dic = {0:1}
-        for i in nums:
-            prf_sum.append(prf_sum[-1] + i)
-            if prf_sum[-1] - goal in ct_dic:
-                ct += ct_dic[prf_sum[-1] - goal]
-            if prf_sum[-1] in ct_dic:
-                ct_dic[prf_sum[-1]] += 1
-            else:
-                ct_dic[prf_sum[-1]] = 1
-       
-       
-        return ct
+        # calculate the number of windows whose sum is less than the goal 
+        # and whose sum is less than (goal-1) then substract these two to get the windows whose sum is exactly goal
+
+        def sum_less_than(goal):
+            nonlocal nums
+            _sum, i = 0, 0
+            total = 0
+
+            for j in range(len(nums)):
+                _sum += nums[j]
+
+                while i < len(nums) and _sum > goal:
+                    _sum -= nums[i]
+                    i += 1
+
+                total += j - i + 1
+            return total
+
+        # print(sum_less_than(-1))
+        return sum_less_than(goal) - sum_less_than(goal-1) if goal else sum_less_than(goal)
+
+                
+
+
+
