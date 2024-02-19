4574dc28876ee8370d3e20c4b6fcaeace844e551
kal <93663038+kalkidan-hub@users.noreply.github.com>
2024-02-19 19:12:43 +0300

    Add solution for Time Needed to Buy Tickets

--- Datastructure/camp1/leetcode/time-needed-to-buy-tickets.py
+++ Datastructure/camp1/leetcode/time-needed-to-buy-tickets.py
@@ -1,14 +1,7 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        # simulating the process
-        # worst case the loop runs 100*100 times
+        # optimized way
         res = 0
-        while tickets[k]:
-            for i in range(len(tickets)):
-                if tickets[i]:
-                    tickets[i] -= 1
-                    res += 1
-                if i == k and tickets[i] == 0:
-                    break
-
+        for i in range(len(tickets)):
+            res += min(tickets[i],tickets[k] - (i > k))
         return res
